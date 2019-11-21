4f233b976948fcd6334097b886429a3426a413a6
jreyarijon <56083986+jreyarijon@users.noreply.github.com>
2019-11-21 17:29:26 +0100

    Add files via upload
    Completado

---
+++ src/bol10_2/Bol10_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol10_2;
+
+/**
+ *
+ * @author Josemolamazo
+ */
+public class Bol10_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Xogo adivinar = new Xogo();
+        adivinar.Xogar();
+    }
+    
+}
